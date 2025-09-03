871e8c3d11c50771682cd5da4574d61b38eef24b
arthur <arthurswitch3@gmail.com>
2025-09-03 15:56:53 -0300

    add unzip&remove function

--- tidy.sh
+++ tidy.sh
@@ -1,1 +1,23 @@
 #!/bin/bash
+
+fileName="$1"
+
+if [ -z "$fileName" ]; then
+  echo "No zip file mentioned."
+  exit 1
+fi
+
+case "$fileName" in
+*.zip)
+  unzip "$fileName"
+  status=$?
+
+  if [ $status -eq 0 ]; then
+    rm "$fileName"
+    echo "$fileName unzipped."
+    echo "$fileName removed."
+  else
+    exit 1
+  fi
+  ;;
+esac
